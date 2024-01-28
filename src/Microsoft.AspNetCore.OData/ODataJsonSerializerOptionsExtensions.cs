8cc70b0e3291eeb41b5dd62488e86a512522ae4d
Callum Marshall <callummarshall9@googlemail.com>
2024-01-28 18:47:17 +0000

    Expose JsonSerializer to allow serializing responses easier.

---
+++ src/Microsoft.AspNetCore.OData/ODataJsonSerializerOptionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.AspNetCore.OData.Query.Wrapper;
+using Microsoft.AspNetCore.OData.Results;
+
+namespace Microsoft.AspNetCore.OData
+{
+    public static class ODataJsonSerializerOptionsExtensions
+    {
+        public static JsonSerializerOptions GetSerializerOptions() {
+            JsonSerializerOptions serializerOptions = new JsonSerializerOptions();
+
+            serializerOptions.Converters.Add(new SelectExpandWrapperConverter());
+            serializerOptions.Converters.Add(new PageResultValueConverter());
+            serializerOptions.Converters.Add(new DynamicTypeWrapperConverter());
+            serializerOptions.Converters.Add(new SingleResultValueConverter());
+
+            return serializerOptions;
+        }
+    }
+}
